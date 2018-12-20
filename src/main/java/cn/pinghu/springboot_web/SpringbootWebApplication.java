a678bef9f2d45483d67417167d190126140888ed
zzZZxiaoshen <644921352@qq.com>
2018-12-20 23:18:40 +0800

    commit

---
+++ src/main/java/cn/pinghu/springboot_web/SpringbootWebApplication.java
@@ -0,0 +1,14 @@
+package cn.pinghu.springboot_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWebApplication.class, args);
+	}
+
+}
+
